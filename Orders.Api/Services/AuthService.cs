e36f4d167760a84421ca95108199b995470e3473
Maksim <jdwizzard@gmail.com>
2024-02-08 22:29:49 +0100

    Add project files.

---
+++ Orders.Api/Services/AuthService.cs
@@ -0,0 +1,41 @@
+﻿namespace Orders.Api.Services
+{
+    public class AuthService
+    {
+        public List<string> Users { get; private set; }
+
+        public AuthService()
+        {
+            Users = new List<string>
+            {
+                "Smith",
+                "Johnson",
+                "Williams",
+                "Jones",
+                "Brown",
+                "Davis",
+                "Miller",
+                "Wilson",
+                "Moore",
+                "Taylor"
+            };
+        }
+
+        public bool? Login(string lastName)
+        {
+            var result = false;
+
+            if (string.IsNullOrEmpty(lastName))
+            {
+                return false;
+            }
+
+            if (Users.Contains(lastName))
+            {
+                result =  true;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Orders.Tests/AuthServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using Orders.Api.Services;
+
+namespace Orders.Tests
+{
+    public class AuthServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        
+        [TestCase("Wilson")]
+        [TestCase("Moore")]
+        [TestCase("Taylor")]
+        public void Login_ShouldReturnTrue(string lastName)
+        {
+            //Arrange
+            var AuthService = new AuthService();
+            
+            //Act
+            var result = AuthService.Login(lastName);
+
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase("FalseName")]
+        public void Login_ShouldReturnFalse(string lastName)
+        {
+            //Arrange
+            var AuthService = new AuthService();
+
+            //Act
+            var result = AuthService.Login(lastName);
+
+            //Assert
+            Assert.IsFalse(result);
+
+        }
+    }
+}
